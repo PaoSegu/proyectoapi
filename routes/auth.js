c2ab2cc3fd7ee9ef0bfea5be6c9cd6397c319393
Paola Segura <paolacardona.0819@gmail.com>
2024-08-22 08:14:43 -0500

    cambio server y auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,79 +1,75 @@
-const express = require("express");
-const router = express.Router();
-const bcrypt = require( "bcrypt");
-const jwt = require ( "jsonwebtoken");
-const User = require("../models/User");
+const express = require('express'); 
+const router = express.Router(); 
+const bcrypt = require('bcrypt'); 
+const jwt = require('jsonwebtoken');
+const User = require ('../models/User')
 
-router.post("/register", async (req, res) => {
+router.post('/register', async(req, res) =>{ 
     const {username, email, password} = req.body;
 
-    try{
-        let user = await User.findOne({where: {email}})
-        if(user){
-            return res.status(400).json({msg: "Usuario ya exixte"})
+     try{ 
+        let user = await User.findOne({where: {email}}); 
+        if(user){ 
+         return res.status(400).json({msg: 'Usuario ya existe'})
+
+        } 
+
+         const salt = await bcrypt.genSalt(10); 
+         const hashesPassword = await bcrypt.hash(password, salt) 
+         
+         user = await User.create({ 
+            username, 
+            email, 
+            password: hashesPassword 
+        }) 
+        
+        const payload ={ 
+            user:{ 
+                id: user.id 
+            } 
+        } 
+        
+        jwt.sign(payload, 'secret', {expiresIn: 360000}, (err, token) =>{ 
+            if (err) throw err; 
+            res.json({token})
+         }
+        
+        ) }
+        
+        catch (err){ 
+            console.error(err.message); 
+            res.status(500).send('Error al registrar usuario') 
         }
+     });
+     
 
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
+router.post('/login', async(req, res) => { 
+    const {email, password} = req.body; 
+    
+    try{ let user = await User.findOne({where: {email}}); 
+    if(!user){ 
+        return res.status(400).json({msg: 'Usuario no encontrado'}) 
+    } 
+    
+    const isMatch = await bcrypt.compare(password, user.password); 
+    if(!isMatch){ 
+        return res.status(400).json({msg: 'ContraseÃ±a incorrecta'}) 
+    } 
+    
+    const payload ={ 
+        user:{ 
+            id: user.id 
+        } 
+    } 
+    
+        jwt.sign(payload, 'secret', {expiresIn: 360000}, (err, token) =>{ 
+            if (err) throw err; 
+            res.json({token}) 
+        }) 
+    
+    }catch (err){ 
+        console.error(err.message); 
+        res.status(500).send('Error en el servidor '); 
+    } 
 
-        user = await User.create({
-            username,
-            email,
-            password: hashedPassword
-        })
-
-        const payload = {
-            user: {
-                id: user.id
-            }
-        }
-
-        jwt.sign(payload, "secret", { expiresIn: 360000 }, (err, token) => {
-            if (err) throw err;
-            res.json({ token });
-        }
-
-        )
-
-    }catch(err){
-        console.error(err.message)
-        res.status(500).send("Error al tegistrar usuario")
-    }
-
-})
-
-router.post("/login", async (req, res) => {
-    const {email, password} = req.body;
-
-    try{
-        let user = await User.findOne({where: {email}})
-        if(!user){
-            return res.status(400).json({msg: "Usuario no encontrado"})
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password)
-        if(!isMatch){
-            return res.status(400).json({msg: "Contraseña incorrecta"})
-        }
-
-        const payload = {
-            user: {
-                id: user.id
-            }
-        }
-
-        jwt.sign(payload, "secret", { expiresIn: 360000 }, (err, token) => {
-            if (err) throw err;
-            res.json({ token });
-        })
-
-
-
-    }catch(err){
-        console.error(err.message)
-        res.status(500).send("Error en el servidor")
-    }   
-
-});
-
-module.exports = router;
+}); module.exports = router; 
--- server.js
+++ server.js
@@ -1,42 +1,41 @@
-const express = require('express');
-const cors = require('cors');
-const {connectDB} = require('./config/db');
-const bodyparser = require('body-parser');
-require('dotenv').config()
-const path = require('path')
-const authRoutes = require('./routes/auth');
+const express = require('express'); 
+const cors = require('cors'); 
+const {connectDB} = require('./config/db'); 
+const bodyParser = require('body-parser'); 
+require('dotenv').config() 
+const path = require('path') 
 const app = express(); 
 
-connectDB();
+connectDB() 
 
-app.use(cors());
-app.use(bodyparser.json());
+app.use(cors()); 
+app.use(bodyParser.json()) 
 
-app.use(express.static,(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'public'))) 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+app.get('/', (req, res)=>{ 
+    res.sendFile(path.join(__dirname,'public', 'index.html')) 
+}) 
+
+app.use('/api/auth', require('./routes/auth')); 
+
+const PORT = process.env.PORT || 5000; 
+
+app.listen(PORT, async() =>{ 
+    console.log(`Server is running on port ${PORT}`) 
+    const {sequelize} = require('./config/db') 
+    try{ 
+        await sequelize.sync(); 
+        console.log('Database synced') 
+    } catch(err){ 
+        console.error('error base de datos', err) 
+    } 
 })
 
-app.use('/api/auth', authRoutes);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, async() =>{
-     console.log(`Server running on port ${PORT}`);
-     const {sequelize} = require ('./config/db')
-     try {
-         await sequelize.sync()
-         console.log('Databsse synced' )
-     } catch (err) {
-         console.error('Error base de datos', err)
-         
-     }
 
 
 
- 
-})
+
 
 
 
